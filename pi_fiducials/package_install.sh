c1c733f34fd4513fd640e2538a921b18257833b2
David Tarazi <davidtarazi@me.com>
2021-04-15 11:54:45 -0700

    updated dep install

--- pi_fiducials/package_install.sh
+++ pi_fiducials/package_install.sh
@@ -1,21 +1,25 @@
-# updates, dependencies, and optimizations
-sudo apt-get update && apt-get upgrade
-# cmake for configuring opencv
-sudo apt-get install build-essential cmake pkg-config
-# allows us to load images, video streams
-sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get install libxvidcore-dev libx264-dev
-# GUI packages
-sudo apt-get install libfontconfig1-dev libcairo2-dev
-sudo apt-get install libgdk-pixbuf2.0-dev libpango1.0-dev
-sudo apt-get install libgtk2.0-dev libgtk-3-dev
-# optimizations
-sudo apt-get install libatlas-base-dev gfortran
-sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-103
-sudo apt-get install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
-# python3 header files for compiling opencv
-sudo apt-get install python3-dev
+
+# # Mostly adapted from this tutorial:
+# # https://www.pyimagesearch.com/2019/09/16/install-opencv-4-on-raspberry-pi-4-and-raspbian-buster/
+
+# # updates, dependencies, and optimizations
+# sudo apt-get update && apt-get upgrade
+# # cmake for configuring opencv
+# sudo apt-get install build-essential cmake pkg-config
+# # allows us to load images, video streams
+# sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
+# sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+# sudo apt-get install libxvidcore-dev libx264-dev
+# # GUI packages
+# sudo apt-get install libfontconfig1-dev libcairo2-dev
+# sudo apt-get install libgdk-pixbuf2.0-dev libpango1.0-dev
+# sudo apt-get install libgtk2.0-dev libgtk-3-dev
+# # optimizations
+# sudo apt-get install libatlas-base-dev gfortran
+# sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-103
+# sudo apt-get install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
+# # python3 header files for compiling opencv
+# sudo apt-get install python3-dev
 
 # install opencv and picamera with numpy dependencies
 wget https://bootstrap.pypa.io/get-pip.py
@@ -23,6 +27,8 @@ sudo python get-pip.py
 sudo python3 get-pip.py
 sudo rm -rf ~/.cache/pip
 pip install --upgrade pip
+pip install -U setuptools pip
+pip install --upgrade pip setuptools wheel
 pip install numpy
 pip install "picamera[array]"
 pip install opencv-contrib-python
