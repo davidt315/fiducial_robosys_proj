602c4cdeb3b4268f0fa8ab54910a881985fcca25
David Tarazi <davidtarazi@me.com>
2021-04-21 18:05:12 -0700

    updated

--- pi_fiducials/package_install.sh
+++ pi_fiducials/package_install.sh
@@ -1,58 +1,36 @@
 
-# # Mostly adapted from this tutorial:
-# # https://www.pyimagesearch.com/2019/09/16/install-opencv-4-on-raspberry-pi-4-and-raspbian-buster/
+#!/usr/bin/env bash
+# Mostly adapted from this tutorial:
+# https://www.pyimagesearch.com/2019/09/16/install-opencv-4-on-raspberry-pi-4-and-raspbian-buster/
 
-# # updates, dependencies, and optimizations
-# sudo apt-get update && apt-get upgrade
-# # cmake for configuring opencv
-# sudo apt-get install build-essential cmake pkg-config
-# # allows us to load images, video streams
-# sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
-# sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-# sudo apt-get install libxvidcore-dev libx264-dev
-# # GUI packages
-# sudo apt-get install libfontconfig1-dev libcairo2-dev
-# sudo apt-get install libgdk-pixbuf2.0-dev libpango1.0-dev
-# sudo apt-get install libgtk2.0-dev libgtk-3-dev
-# # optimizations
-# sudo apt-get install libatlas-base-dev gfortran
-# sudo apt-get install libhdf5-dev libhdf5-serial-dev libhdf5-103
-# sudo apt-get install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
-# # python3 header files for compiling opencv
-# sudo apt-get install python3-dev
+# updates, dependencies, and optimizations
+sudo apt-get update && sudo apt-get upgrade -y
+# cmake for configuring opencv
+sudo apt-get install -y build-essential cmake pkg-config
+# allows us to load images, video streams
+sudo apt-get install -y libjpeg-dev libtiff5-dev libjasper-dev libpng-dev
+sudo apt-get install -y libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get install -y libxvidcore-dev libx264-dev
+# GUI packages
+sudo apt-get install -y libfontconfig1-dev libcairo2-dev
+sudo apt-get install -y libgdk-pixbuf2.0-dev libpango1.0-dev
+sudo apt-get install -y libgtk2.0-dev libgtk-3-dev
+# optimizations
+sudo apt-get install -y libatlas-base-dev gfortran
+sudo apt-get install -y libhdf5-dev libhdf5-serial-dev libhdf5-103
+sudo apt-get install -y libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
+# python3 header files for compiling opencv
+sudo apt-get install -y python3-dev
 
 # install opencv and picamera with numpy dependencies
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
-sudo python3 get-pip.py
-sudo rm -rf ~/.cache/pip
-pip install --upgrade pip
-pip install -U setuptools pip
-pip install --upgrade pip setuptools wheel
-pip install numpy
-pip install "picamera[array]"
-pip install opencv-contrib-python
+sudo pip install --upgrade pip
+sudo pip install -U setuptools pip
+sudo pip install --upgrade pip setuptools wheel
+sudo pip install numpy
+sudo pip install "picamera[array]"
+sudo pip install opencv-python
+sudo pip install aruco
 
-# compile opencv
-workon cv
-cd ~/opencv
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-    -D CMAKE_INSTALL_PREFIX=/usr/local \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
-    -D ENABLE_NEON=ON \
-    -D ENABLE_VFPV3=ON \
-    -D BUILD_TESTS=OFF \
-    -D INSTALL_PYTHON_EXAMPLES=OFF \
-    -D OPENCV_ENABLE_NONFREE=ON \
-    -D CMAKE_SHARED_LINKER_FLAGS=-latomic \
-    -D BUILD_EXAMPLES=OFF ..
-
-# enable on all 4 cores
-make -j4
-sudo make install
-sudo ldconfig
 
 
 
