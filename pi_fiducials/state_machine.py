44dbb6a52071c4a9f68330e39725f8767ed1103a
David Tarazi <dtarazi@olin.edu>
2021-05-06 03:01:56 +0100

    bugs

--- pi_fiducials/state_machine.py
+++ pi_fiducials/state_machine.py
@@ -6,14 +6,21 @@ class Robot():
     def __init__(self):
         # motor pin definitions (in1, in2, pwm)
         self.Rmotor = Motor(38, 37, 12)
-        
-        self.setup_gpio()
-
+	self.setup_gpio()
         self.state = IdleState()
-        
-    def setup_gpio(self):
 
-    
+    def setup_gpio(self):
+	# set motor pins
+        io.setmode(io.BCM)
+        io.setup(self.Rmotor.pins[0], io.OUT)
+        io.setup(self.Rmotor.pins[1], io.OUT)
+        io.setup(self.Rmotor.pins[2], io.OUT)
+
+        # define starting output of motor pins (forward with 0 speed)
+        io.output(self.Rmotor.pins[0], io.HIGH)
+        io.output(self.Rmotor.pins[1], io.LOW)
+        self.Rmotor.start_pwm()
+
     def change_state(self, detections):
         pass
 
@@ -25,13 +32,15 @@ class Motor():
     def __init__(self, in1, in2, pwm):
         self.pins = (in1, in2, pwm)
         self.speed = 0
-        
+
     def start_pwm(self):
         self.pwm_control = io.PWM(self.pins[2], 1000)
         self.pwm_control.start(self.speed)
+	print("started pwm")
 
     def set_speed(self, percent):
         self.pwm_control.ChangeDutyCycle(percent)
+	print("speed changed")
 
 # super class
 class State():
@@ -49,12 +58,13 @@ class State():
 class IdleState(State):
     def __init__(self):
         print('entering idle state')
-    
+
     def change_state(self, detections):
         pass
 
     def act(self, detections, Rmotor):
         Rmotor.set_speed(50)
+	print("completed action")
 
 
 class FollowState(State):
@@ -65,4 +75,4 @@ class SearchState(State):
 
 class FindState(State):
     pass
-    
+
