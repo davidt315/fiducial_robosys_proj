18ba7ef4d94a04c34b7c01ad25b799ddfb44ed27
David Tarazi <davidtarazi@me.com>
2021-05-07 12:20:13 -0700

    added git ignore, class and state updates

--- pi_fiducials/execute.py
+++ pi_fiducials/execute.py
@@ -7,89 +7,96 @@ import logging
 import RPi.GPIO as io 
 from state_machine import Robot
 
-# Setup fiducial dictionary and params
-DICTIONARY = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50) # our marker dictionary
-tag = np.zeros((300, 300, 1), dtype="uint8")
-cv2.aruco.drawMarker(DICTIONARY, 10, 300, tag, 1)
-cv2.imwrite("images/test_tag.png", tag)
-PARAMETERS = cv2.aruco.DetectorParameters_create() # default params
+class RunCar():
+	def __init__(self):
+		# Setup fiducial dictionary and params
+		self.DICTIONARY = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50) # our marker dictionary
+		self.PARAMETERS = cv2.aruco.DetectorParameters_create() # default params
+		self.tag = np.zeros((300, 300, 1), dtype="uint8")
+		cv2.aruco.drawMarker(self.DICTIONARY, 10, 300, self.tag, 1)
+		cv2.imwrite("images/test_tag.png", self.tag)
+		
+		# picamera params
+		self.RESOLUTION = (1008,1008)
+		self.FRAMERATE = 30
 
-# picamera params
-RESOLUTION = (1008,1008)
-FRAMERATE = 30
+		# create VideoWriter object
+		fourcc = cv2.VideoWriter_fourcc(*'XVID')
+		self.vid = cv2.VideoWriter('images/output.avi',fourcc, 20.0, self.RESOLUTION)
 
-# create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
-vid = cv2.VideoWriter('images/output.avi',fourcc, 20.0, RESOLUTION)
 
-def find_markers(frame):
-	gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	corners, ids, _ = cv2.aruco.detectMarkers(gray_frame, DICTIONARY, parameters=PARAMETERS)
+	def find_markers(self, frame):
+		gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		corners, ids, _ = cv2.aruco.detectMarkers(gray_frame, self.DICTIONARY, parameters=self.PARAMETERS)
 
-	result = set()
-	if ids is None:
-		vid.write(frame)
+		result = [[], [], []]
+		if ids is None:
+			self.vid.write(frame)
+			return result
+
+		ids = ids.flatten()
+		for (markerCorner, markerID) in zip(corners, ids):
+			# extract the marker corners
+			corners = markerCorner.reshape((4, 2))
+			(topLeft, topRight, bottomRight, bottomLeft) = corners
+
+			# convert each of the (x, y)-coordinate pairs to integers
+			topRight = (int(topRight[0]), int(topRight[1]))
+			bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+			bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+			topLeft = (int(topLeft[0]), int(topLeft[1]))
+			# center coords
+			x_cent = int((topLeft[0] + bottomRight[0])/2.0)
+			y_cent = int((topLeft[1] + bottomRight[1])/2.0)
+			width = abs(topRight[0] - bottomLeft[0])
+
+			# annotate the frame
+			# draw the bounding box of the ArUCo detection
+			cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
+			cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
+			cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
+			cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
+			cv2.circle(frame, (x_cent, y_cent), 4, (0, 0, 255), -1)
+			# draw the ArUco marker ID on the frame
+			cv2.putText(frame, str(markerID), (topLeft[0], topLeft[1] - 15),
+						cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+			# add result
+			result[0].append(int(markerID))
+			result[1].append(x_cent)
+			result[2].append(width)
+		# write frame to vid
+		self.vid.write(frame)
 		return result
 
-	ids = ids.flatten()
-	for (markerCorner, markerID) in zip(corners, ids):
-		# extract the marker corners
-		corners = markerCorner.reshape((4, 2))
-		(topLeft, topRight, bottomRight, bottomLeft) = corners
 
-		# convert each of the (x, y)-coordinate pairs to integers
-		topRight = (int(topRight[0]), int(topRight[1]))
-		bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
-		bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
-		topLeft = (int(topLeft[0]), int(topLeft[1]))
-		# center coords
-		x_cent = int((topLeft[0] + bottomRight[0])/2.0)
-		y_cent = int((topLeft[1] + bottomRight[1])/2.0)
-		width = abs(topRight[0] - bottomLeft[0])
+	def execute(self):
+		# create the camera
+		camera = picamera.PiCamera()
+		camera.resolution = self.RESOLUTION # (1008,1008)
+		camera.framerate = self.FRAMERATE # 30
 
-		# annotate the frame
-		# draw the bounding box of the ArUCo detection
-		cv2.line(frame, topLeft, topRight, (0, 255, 0), 2)
-		cv2.line(frame, topRight, bottomRight, (0, 255, 0), 2)
-		cv2.line(frame, bottomRight, bottomLeft, (0, 255, 0), 2)
-		cv2.line(frame, bottomLeft, topLeft, (0, 255, 0), 2)
-		cv2.circle(frame, (x_cent, y_cent), 4, (0, 0, 255), -1)
-		# draw the ArUco marker ID on the frame
-		cv2.putText(frame, str(markerID), (topLeft[0], topLeft[1] - 15),
-					cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+		# create the Robot
+		car = Robot()
 
-		# add result
-		result.add((id, x_cent, y_cent, width))
-	# write frame to vid
-	vid.write(frame)
-	return result
+		rawCapture = picamera.array.PiRGBArray(camera, size=self.RESOLUTION)
+		try:
+			for capture in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+				frame = capture.array
+				markers = self.find_markers(frame)
+				
+				print(markers)
+				car.change_state(markers)
+				car.act(markers)
+				rawCapture.truncate(0)
 
+		except KeyboardInterrupt:
+			logging.exception("Keyboard interruption: exiting.")
+		finally:
+			camera.close()
+			self.vid.release()
+			io.cleanup()
 
-def execute():
-	# create the camera
-	camera = picamera.PiCamera()
-	camera.resolution = RESOLUTION # (1008,1008)
-	camera.framerate = FRAMERATE # 30
-
-	# create the Robot
-	car = Robot()
-
-	rawCapture = picamera.array.PiRGBArray(camera, size=RESOLUTION)
-	try:
-		for capture in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-			frame = capture.array
-			markers = find_markers(frame)
-			
-			print(markers)
-			car.act(markers)
-			rawCapture.truncate(0)
-
-	except KeyboardInterrupt:
-		logging.exception("Keyboard interruption: exiting.")
-	finally:
-		camera.close()
-		vid.release()
-		io.cleanup()
-
-
-execute()
+if __name__ == "__main__":
+	script = RunCar()
+	script.execute()
--- pi_fiducials/state_machine.py
+++ pi_fiducials/state_machine.py
@@ -1,55 +1,68 @@
 import numpy as np
-import RPi.GPIO as io 
-
+from scipy.interpolate import interp1d 
+import time
+import RPi.GPIO as io
 
 class Robot():
     def __init__(self):
         # motor pin definitions (in1, in2, pwm)
         self.Rmotor = Motor(15, 16, 12)
+        self.Lmotor = Motor(21, 22, 13)
         self.setup_gpio()
         self.state = IdleState()
 
     def setup_gpio(self):
-	# set motor pins
+	    # set motor pins
         io.setmode(io.BOARD)
         io.setup(self.Rmotor.pins[0], io.OUT)
         io.setup(self.Rmotor.pins[1], io.OUT)
         io.setup(self.Rmotor.pins[2], io.OUT)
+        io.setup(self.Lmotor.pins[0], io.OUT)
+        io.setup(self.Lmotor.pins[1], io.OUT)
+        io.setup(self.Lmotor.pins[2], io.OUT)
 
         # define starting output of motor pins (forward with 0 speed)
         io.output(self.Rmotor.pins[0], io.HIGH)
         io.output(self.Rmotor.pins[1], io.LOW)
+        io.output(self.Lmotor.pins[0], io.HIGH)
+        io.output(self.Lmotor.pins[1], io.LOW)
         self.Rmotor.start_pwm()
+        self.Lmotor.start_pwm()
 
     def change_state(self, detections):
-        pass
+        self.state = self.state.change_state(detections)
 
     def act(self, detections):
-        self.state.act(detections, self.Rmotor)
+        self.state.act(detections, self.Rmotor, self.Lmotor)
 
 
 class Motor():
     def __init__(self, in1, in2, pwm):
         self.pins = (in1, in2, pwm)
         self.speed = 0
+        self.base_speed = 50
 
     def start_pwm(self):
         self.pwm_control = io.PWM(self.pins[2], 500)
         self.pwm_control.start(self.speed)
 
-    def set_speed(self, percent):
+    def set_speed(self, speed):
+        self.speed = speed
         self.pwm_control.ChangeDutyCycle(self.speed)
-        print("speed changed")
+
 
 # super class
 class State():
     def __init__(self):
-        pass
+        # fiducial ID's 
+        self.follow_id = 17
+        self.search_id = 24
+        self.search_bool = False
+        # PID inputs
+        self.setpoint = 504
+        self.cutoff_width = 300
 
-    def change_state(self, markers):
-        pass
-
-    def act(self):
+    def change_state(self, detections):
         pass
 
 
@@ -59,20 +72,91 @@ class IdleState(State):
         print('entering idle state')
 
     def change_state(self, detections):
-        pass
+        # search fiducial detected and unsearched
+        if not self.search_bool and self.search_id in detections[0]:
+            return SearchState()
+        # some other condition, enter find state
+        return FindState()
 
-    def act(self, detections, Rmotor):
-        Rmotor.speed = 80
-        Rmotor.set_speed(Rmotor.speed)
-        print(f"speed: {Rmotor.speed}")
+    def act(self, detections, Rmotor, Lmotor):
+        # check if the motor speeds were already set
+        if Rmotor.speed is 0 and Lmotor.speed is 0:
+            return
+
+        # set the motor speeds to 0
+        Rmotor.speed = 0
+        Lmotor.speed = 0
+        Rmotor.set_speed()
+        Lmotor.set_speed()
+        print(f"Rmotor speed: {Rmotor.speed}")
+
 
 
 class FollowState(State):
-    pass 
+    def __init__(self):
+        super().__init__()
+        self.count = 0
+        self.cum_error = 0
+        self.last_error = 0
+        self.last_time = time.time()*1000.0
+        self.kp = 0.1
+        self.ki = 0.01
+        self.kd = 0.0001
 
+    def change_state(self, detections):
+        # no detections or only detect search point that has already been searched:
+        if (self.follow_id not in detections[0]):
+            self.count += 1
+            if self.count >= 5:
+                return FindState()
+            return self
+
+        # search fiducial detected and not searched
+        if self.search_id in detections[0] and not self.search_bool:
+            return SearchState()
+        
+        # must have seen the follow tag again, reset count and continue
+        self.count = 0
+        return self
+        
+
+    def act(self, detections, Rmotor, Lmotor):
+        # if for a frame we don't see the follow tag, skip the iteration this loop
+        if self.follow_id not in detections[0]:
+            return
+
+        # if too close, don't move
+        width = detections[2][detections[0].index(self.follow_id)]
+        if width > self.cutoff_width:
+            Lmotor.set_speed(0)
+            Rmotor.set_speed(0)
+            self.last_error = 0
+            self.last_time = time.time()*1000.0
+            return
+
+        x_pos = detections[1][detections[0].index(self.follow_id)]
+        error = self.setpoint - x_pos
+        elapsed_time = time.time()*1000.0 - self.last_time
+        self.cum_error += error * elapsed_time
+        rate_error = (error - self.last_error) / elapsed_time
+        output = self.kp*error + self.ki*self.cum_error + self.kd*rate_error
+        
+        Lmotor.speed = Lmotor.base_speed - int(output)
+        Rmotor.speed = Rmotor.base_speed + int(output)
+        Lmotor.set_speed()
+        Rmotor.set_speed()
+        
+        self.last_error = 0
+        self.last_time = time.time()*1000.0
+
+
+
+        
 class SearchState(State):
-    pass 
+    def change_state(self):
+        return IdleState()
 
 class FindState(State):
-    pass
+    def change_state(self):
+        return IdleState()
 
